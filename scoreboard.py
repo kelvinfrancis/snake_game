a6ddf042278fb13b60c6e739d4fa84fa6c1d4564
Kelvin Moquete <kelvinmoquete0597@gmail.com>
2025-10-23 09:59:22 -0400

    high score changes

--- scoreboard.py
+++ scoreboard.py
@@ -7,20 +7,23 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.penup()
-        self.goto(0, 270)
+        self.goto(0, 260)
         self.color("white")
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
